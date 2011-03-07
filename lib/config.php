f6120dad548a7ffdcc5651414fa2183cd84096f0
Amit Snyderman <amit@amitsnyderman.com>
2011-03-07 15:38:42 -0500

    Dial plan bug fix

--- lib/config.php
+++ lib/config.php
@@ -3,7 +3,7 @@
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', true);
 
-const DB_HOST = '127.0.0.1';
-const DB_USER = 'root';
-const DB_PASS = '';
-const DB_NAME = 'voitp';
+define('DB_HOST', '127.0.0.1');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'voitp');
