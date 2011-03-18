d2f5ba648e7bcd3fc8e9ac179b61118da06fdd4d
Amit Snyderman <amit@amitsnyderman.com>
2011-03-18 13:41:01 -0400

    Get dial out to work and rate limit the calls

--- examples/thankyou.php
+++ examples/thankyou.php
@@ -1,10 +1,11 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
 <html>
-	<head>
-		<title>Thank You</title>
-	</head>
-	<body>
-	Thanks for registering as an expert. Prepare to be called!
-	</body>
+<head>
+	<title>Thank You</title>
+</head>
+<body>
+	<p>Thanks for registering as an expert. Prepare to be called!</p>
+	<p>Please add <code>212-555-1212</code> to your address book to identify calls from <em>Que?</em>.</p>
+</body>
 </html>
